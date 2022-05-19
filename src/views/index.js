d3f1980e988ab151ed9d0622c986c355f2f0626a
César Freitas <cesarfreitascontato@gmail.com>
2022-05-18 22:40:13 -0300

    feat: novas divs no index e manipulacao de dom js

--- src/views/index.js
+++ src/views/index.js
@@ -8,3 +8,37 @@ moreTextBtn.addEventListener('click', (e)=>{
         moreTextBtn.style.display="none"
     }
 })
+
+const discuss = document.querySelector('.discuss');
+const discussBtn = document.querySelector('.discussBtn');
+const newTopic = document.querySelector('.newTopic');
+
+discussBtn.addEventListener('click', (e)=>{
+    discuss.style.display="none"
+    newTopic.style.display="flex"
+})
+
+const newTopicBtn = document.querySelector('.newTopicBtn');
+const topicCreated = document.querySelector('.topicCreated');
+const assunto = document.querySelector('.assunto');
+const conteudo = document.querySelector('.conteudo');
+let assuntoMsg;
+
+newTopicBtn.addEventListener('click', (e)=>{
+    assuntoMsg = assunto.value;
+    console.log(assuntoMsg)
+    newTopic.style.display="none"
+    topicCreated.style.display="flex"
+})
+
+
+const topicCreatedBtn = document.querySelector('.topicCreatedBtn'); 
+
+topicCreatedBtn.addEventListener('click', (e)=>{
+    topicCreated.style.display="none"
+    newTopic.style.display="flex"
+})
+
+
+
+
